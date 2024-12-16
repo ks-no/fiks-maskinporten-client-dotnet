36bcda6bfeaa8c91984a715eb1c2c8482bbce740
Kolbein Toreson Foldøy <kolbeinfoldoy@gmail.com>
2024-12-16 10:54:58 +0100

    Add kid in JwtRequestTokenGenerator for cert + .NET 8

--- KS.Fiks.Maskinporten.Client/Jwt/JwtRequestTokenGenerator.cs
+++ KS.Fiks.Maskinporten.Client/Jwt/JwtRequestTokenGenerator.cs
@@ -15,13 +15,13 @@ namespace Ks.Fiks.Maskinporten.Client.Jwt
         private const int JwtExpireTimeInMinutes = 2;
         private const string DummyKey = ""; // Required by encoder, but not used with RS256Algorithm
 
-        private readonly JwtEncoder encoder;
-        private readonly X509Certificate2 certificate;
-        private readonly string keyIdentifier;
-        private readonly RSA privateKey;
-        private readonly RSA publicKey;
+        private readonly JwtEncoder _encoder;
+        private readonly X509Certificate2 _certificate;
+        private readonly string _keyIdentifier;
 
-        private static IDictionary<string, object> CreateJwtPayload(TokenRequest tokenRequest, MaskinportenClientConfiguration configuration)
+        private static IDictionary<string, object> CreateJwtPayload(
+            TokenRequest tokenRequest,
+            MaskinportenClientConfiguration configuration)
         {
             var jwtData = new JwtData();
 
@@ -51,12 +51,13 @@ namespace Ks.Fiks.Maskinporten.Client.Jwt
             return jwtData.Payload;
         }
 
-        public JwtRequestTokenGenerator(X509Certificate2 certificate)
+        public JwtRequestTokenGenerator(X509Certificate2 certificate, string keyIdentifier = null)
         {
-            this.certificate = certificate;
-            this.privateKey = certificate.GetRSAPrivateKey();
-            this.publicKey = certificate.GetRSAPublicKey();
-            this.encoder = new JwtEncoder(
+            _certificate = certificate;
+            var privateKey = certificate.GetRSAPrivateKey();
+            var publicKey = certificate.GetRSAPublicKey();
+            _keyIdentifier = keyIdentifier;
+            _encoder = new JwtEncoder(
                 new RS256Algorithm(publicKey, privateKey),
                 new JsonNetSerializer(),
                 new JwtBase64UrlEncoder());
@@ -64,10 +65,8 @@ namespace Ks.Fiks.Maskinporten.Client.Jwt
 
         public JwtRequestTokenGenerator(RSA publicKey, RSA privateKey, string keyIdentifier = null)
         {
-            this.keyIdentifier = keyIdentifier;
-            this.privateKey = privateKey;
-            this.publicKey = publicKey;
-            this.encoder = new JwtEncoder(
+            _keyIdentifier = keyIdentifier;
+            _encoder = new JwtEncoder(
                 new RS256Algorithm(publicKey, privateKey),
                 new JsonNetSerializer(),
                 new JwtBase64UrlEncoder());
@@ -77,7 +76,7 @@ namespace Ks.Fiks.Maskinporten.Client.Jwt
         {
             var payload = CreateJwtPayload(tokenRequest, configuration);
             var header = CreateJwtHeader();
-            var jwt = this.encoder.Encode(header, payload, DummyKey);
+            var jwt = _encoder.Encode(header, payload, DummyKey);
 
             return jwt;
         }
@@ -86,14 +85,15 @@ namespace Ks.Fiks.Maskinporten.Client.Jwt
         {
             Dictionary<string, object> jwtHeaderValues = new Dictionary<string, object>();
 
-            if (certificate != null)
+            if (_certificate != null)
             {
-                jwtHeaderValues.Add("x5c", new List<string>() { Convert.ToBase64String(this.certificate.Export(X509ContentType.Cert)) });
+                jwtHeaderValues.Add("x5c",
+                    new List<string>() { Convert.ToBase64String(_certificate.Export(X509ContentType.Cert)) });
             }
 
-            if (!string.IsNullOrWhiteSpace(keyIdentifier))
+            if (!string.IsNullOrWhiteSpace(_keyIdentifier))
             {
-                jwtHeaderValues.Add("kid", keyIdentifier);
+                jwtHeaderValues.Add("kid", _keyIdentifier);
             }
 
             return jwtHeaderValues;
